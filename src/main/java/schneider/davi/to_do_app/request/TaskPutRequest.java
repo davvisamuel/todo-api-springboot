dc7fdc6687f873be36271e3e1249ab2d653b83fe
Davi <davvisamuels@gmail.com>
2025-07-30 20:58:00 -0300

    Fix: Change @NotBlank to @NotNull for the id field.

--- src/main/java/schneider/davi/to_do_app/request/TaskPutRequest.java
+++ src/main/java/schneider/davi/to_do_app/request/TaskPutRequest.java
@@ -1,13 +1,14 @@
 package schneider.davi.to_do_app.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TaskPutRequest {
-    @NotBlank(message = "The field 'id' is required")
+    @NotNull(message = "The field 'id' is required")
     private Long id;
     @NotBlank(message = "The field 'title' is required")
     private String title;
