68ed29b6a9103a149b4b9dee27b98566921c1a9f
Davi <davvisamuels@gmail.com>
2025-07-30 18:59:40 -0300

    feat: add integration test for TaskController.findAll

--- src/test/java/schneider/davi/to_do_app/controller/TaskControllerIT.java
+++ src/test/java/schneider/davi/to_do_app/controller/TaskControllerIT.java
@@ -1,16 +1,20 @@
 package schneider.davi.to_do_app.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
 import schneider.davi.to_do_app.commons.FileUtils;
+import schneider.davi.to_do_app.response.TaskGetResponse;
 import schneider.davi.to_do_app.response.TaskPostResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -23,7 +27,7 @@ class TaskControllerIT {
 
     @Test
     @Order(1)
-    @DisplayName("POST v1/profiles creates a task")
+    @DisplayName("POST /v1/tasks creates a task")
     void save_ReturnsTaskPostResponse_WhenSuccessful() throws IOException {
         var request = fileUtils.readResourceLoader("task/post-request-task-200.json");
 
@@ -33,6 +37,43 @@ class TaskControllerIT {
 
         Assertions.assertThat(responseEntity).isNotNull().hasNoNullFieldsOrProperties();
         Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("GET /v1/tasks returns a list of all tasks")
+    @Sql(value = "/sql/init_two_tasks_db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clean_tasks_db.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void findAll_ReturnsAllTasks_WhenSuccessful() throws IOException {
+        var expectedResponse = fileUtils.readResourceLoader("task/get-response-task-200.json");
+
+        var typeReference = new ParameterizedTypeReference<List<TaskGetResponse>>() {};
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isNotNull();
+
+        responseEntity
+                .getBody()
+                .forEach(taskGetResponse -> Assertions.assertThat(taskGetResponse).hasNoNullFieldsOrProperties());
+
+        JsonAssertions.assertThatJson(responseEntity.getBody())
+                .whenIgnoringPaths("id")
+                .isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("GET /v1/tasks returns a empty list")
+    void findAll_ReturnsEmptyList_WhenSuccessful() throws IOException {
+        var typeReference = new ParameterizedTypeReference<List<TaskGetResponse>>() {};
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isNotNull().isEmpty();
+
     }
 
     private static HttpEntity<String> buildHttpEntity(String request) {
---
+++ src/test/resources/sql/init_two_tasks_db.sql
@@ -0,0 +1,2 @@
+INSERT INTO task (title, description) values ('Go to the market', 'Milk, eggs');
+INSERT INTO task (title, description) values ('To study', 'History, Science');
