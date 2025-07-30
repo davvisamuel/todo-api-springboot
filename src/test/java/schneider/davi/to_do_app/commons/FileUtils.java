555e881db12908445660699c153b0300b8a17082
Davi <davvisamuels@gmail.com>
2025-07-30 15:54:14 -0300

    feat: implement ResourceLoader to read test JSON files

---
+++ src/test/java/schneider/davi/to_do_app/commons/FileUtils.java
@@ -0,0 +1,20 @@
+package schneider.davi.to_do_app.commons;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Component
+public class FileUtils {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public String readResourceLoader(String fileName) throws IOException {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
+    }
+}
